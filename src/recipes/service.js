e1efa658a1150f7ab93dfdb7d409a3672c2f232c
Felipe Gustavo de Meneses Mendes <felipenaoestadisponivel@gmail.com>
2020-04-06 17:03:30 -0300

    Implement getRecipes to call Recipe Puppy API

--- src/recipes/service.js
+++ src/recipes/service.js
@@ -1,8 +1,33 @@
-const getRecipes = async (ingredients) => {
-  const keywords = ingredients.split(',');
-  const result = { keywords };
+import axios from 'axios';
+import { RECIPE_PUPPY_API_URL } from '../config';
 
-  return result;
+function transform(recipe) {
+  const { title, href } = recipe;
+
+  const ingredients = recipe.ingredients
+    .split(',')
+    .map(i => i.trim())
+    .sort();
+
+  return {
+    title,
+    ingredients,
+    link: href
+  };
 }
+
+const getRecipes = async (keywords) => {
+  const query = `?i=${keywords.join(',')}`;
+  const url = `${RECIPE_PUPPY_API_URL}/${query}`;
+
+  const response = await axios.get(url);
+  const { results } = response.data;
+  const recipes = results.map(transform);
+
+  return {
+    keywords,
+    recipes
+  };
+};
 
 export default { getRecipes };
